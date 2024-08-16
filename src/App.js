5ffb84539b1a138c4dfb817cdfcefdcf1dd71a55
Farida Mutai <faridamutai52@gmail.com>
2024-08-16 15:48:24 +0300

    app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import BotCollection from './BotCollection';
+import YourBotArmy from './YourBotArmy';
+import './App.css'; // Import CSS for overall styling
 
 function App() {
+  const [army, setArmy] = useState([]);
+
+  const handleEnlist = (bot) => {
+    setArmy([...army, bot]);
+  };
+
+  const handleRelease = (botId) => {
+    setArmy(army.filter(bot => bot.id !== botId));
+  };
+
+  const handleDischarge = (botId) => {
+    setArmy(army.filter(bot => bot.id !== botId));
+
+    // Delete bot from db.json (backend)
+    fetch(`http://localhost:3000/bots/${botId}`, {
+      method: 'DELETE'
+    });
+  };
+
   return (
     <div className="App">
-      </div>
+      <h1>Bot Battlr</h1>
+      <BotCollection 
+        onEnlist={handleEnlist} 
+        army={army} 
+        onRelease={handleRelease} 
+        onDischarge={handleDischarge} 
+      />
+      <YourBotArmy 
+        army={army} 
+        onRelease={handleRelease} 
+        onDischarge={handleDischarge} 
+      />
+    </div>
   );
 }
 
